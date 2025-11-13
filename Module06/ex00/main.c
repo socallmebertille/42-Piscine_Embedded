66d8a3a90fad4610813332e0578e1f525ab597a0
socallmebertille <sarah.bejard@gmail.com>
2025-11-13 23:01:24 +0100

    Module06/ex00 done

--- Module06/ex00/main.c
+++ Module06/ex00/main.c
@@ -2,12 +2,12 @@
 
 void i2c_init(void)     // Inter-Integrated Circuit | Two-Wire Interface
 {
-    // p.222 |  SCL = CPU_Clock / (16 + 2*TWBR*Prescaler)
+    // p.222 |  SCL = CPU_Clock / (16 + 2*TWBR*Prescaler) = Serial Clock Line = nombre de ticks (impulsions) envoyés par seconde
     // Pour 100kHz avec 16MHz et Prescaler=1 :
     TWBR = 72;          // 100 000 = 16 000 000 / (16 + 2*TWBR*1) <=> TWBR = 72
     
     // p.241 |  TWSR bits 1:0 = Prescaler
-    TWSR = 0;           // 0 = prescaler /1 -> valeur par défaut + meilleure précision
+    TWSR = 0;           // 0 = prescaler /1 -> valeur par défaut (toutes es donnes de la datasheet sont en fonction de ca) + meilleure précision
     
     // p.239:40 |  TWCR - TWI Control Register
     TWCR = (1 << TWEN); // TWEN = 1 : Active le module TWI
@@ -20,54 +20,59 @@ void i2c_start(void)
     //                                 + CPU peut lire statut TWSR ou lire prochaine cmd)
     // TWSTA = 1 (génère START ->  TWSTA must be written to one to transmit a START condition)
     // TWEN = 1 (active TWI -> TWEN must be set to enable the 2-wire Serial Interface)
-    TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN); // step 1 p.225
+    TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);              // step 1 p.225
     
-    // p.221 |  Attendre que TWINT repasse à 1 (indique que l'opération est terminée)
-    while (!(TWCR & (1 << TWINT)));                   // step 2 p.225
-    
-    // Optionnel : vérifier le code de statut
-    // p.226 |  Table 21-3 - Status codes
-    // START transmis : 0x08
-    // Repeated START : 0x10
+    // p.221 |  attendre que TWINT repasse à 1 (indique que l'opération est terminée)
+    while (!(TWCR & (1 << TWINT)));                                // step 2 p.225
 
-    // Step 3: Vérifier que START a été transmis
-    // Status attendu: TW_START (0x08) ou TW_REP_START (0x10)
+    // vérifier que START a bien été transmis
+    // return status values to your computer after each data transmission -> of START
     uart_printstr("START: 0x");
     uart_print_hex(TWSR & 0xF8);
     uart_printstr("\r\n");
-
-    if ((TWSR & 0xF8) != TW_START && (TWSR & 0xF8) != TW_REP_START)
+    
+    // p.227 |  status attendus : TW_START (0x08) ou TW_REP_START (0x10) (version macro de twi.h)
+    if ((TWSR & 0xF8) != TW_START && (TWSR & 0xF8) != TW_REP_START) // step 3.1 p.225
     {
         uart_printstr("ERROR: START failed\r\n");
         return;
     }
 
-    // Step 4: Envoyer l'adresse du slave (AHT20 = 0x38) en mode écriture
-    TWDR = (AHT20_ADDR << 1) | 0;  // Adresse + bit W=0
-    TWCR = (1 << TWINT) | (1 << TWEN);
-    while (!(TWCR & (1 << TWINT)));
+    // https://files.seeedstudio.com/wiki/Grove-AHT20_I2C_Industrial_Grade_Temperature_and_Humidity_Sensor/AHT20-datasheet-2020-4-16.pdf?utm_source=chatgpt.com
+    // -> p.8 |  the 7-bit I2C device address 0x38
+    // envoyer l'adresse du slave (AHT20 = 0x38) en mode écriture
+    // SLA+W = adresse du périphérique esclave + bit W = write = 0 = le maître veut écrire vers l’esclave
+    TWDR = (AHT20_ADDR << 1) | 0;                                   // step 3.2 p.225
+    TWCR = (1 << TWINT) | (1 << TWEN);  // remise des flags = clear + active TWI
+    // p.225 |  attendre que TWINT repasse à 1 (=> SLA+W transmis + ACK/NACK recu)
+    while (!(TWCR & (1 << TWINT)));                                 // step 4 p.225
 
-    // Step 5: Vérifier que le slave a répondu ACK
-    // Status attendu: TW_MT_SLA_ACK (0x18)
+    // vérifier que le slave a répondu ACK
+    // return status values to your computer after each data transmission -> of SLA+W
     uart_printstr("SLA+W: 0x");
     uart_print_hex(TWSR & 0xF8);
     uart_printstr("\r\n");
-
-    if ((TWSR & 0xF8) != TW_MT_SLA_ACK)
+    
+    // p.227 |  status attendus: TW_MT_SLA_ACK (0x18) -> slave a répondu ACK (version macro de twi.h)
+    if ((TWSR & 0xF8) != TW_MT_SLA_ACK)                             // step 5.1 p.225
     {
         uart_printstr("ERROR: Slave NACK\r\n");
         return;
     }
 
+    // on prépare pas la réception données capteur/slave            // step 5.2 p.225
+    // on n'attend la fin de la reception                           // step 6 p.225
+    // on verifie pas le status de retour attendu                   // step 7.1 p.225
+    // car pas demander dans le sujet
 }
 
 void i2c_stop(void)
 {
     // p.224:7 |  Pour générer une condition STOP
     // TWSTO = 1 (génère STOP)
-    TWCR = (1 << TWINT) | (1 << TWSTO) | (1 << TWEN);   // step 7.1 p.225
+    TWCR = (1 << TWINT) | (1 << TWSTO) | (1 << TWEN);                 // step 7.2 p.225
     
-    // Attendre que STOP soit effectivement transmis
+    // attendre que STOP soit effectivement transmis
     while (TWCR & (1 << TWSTO));
 
     uart_printstr("STOP\r\n");
